0e12182b737160b1ade01932ef70955f2ea90ad8
OlehYavoriv <oleh.yavoriv@gmail.com>
2022-12-22 01:13:01 +0200

    Add file with faq

---
+++ src/data/globalAnswers.js
@@ -0,0 +1,32 @@
+export const globalAnswers = [
+  {
+    question: "Lorem ipsum dolor sit amet.",
+    answer:
+      "Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit nostrud pariatur culpa magna in aute.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet.",
+    answer:
+      "Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit nostrud pariatur culpa magna in aute.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet.",
+    answer:
+      "Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit nostrud pariatur culpa magna in aute.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet.",
+    answer:
+      "Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit nostrud pariatur culpa magna in aute.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet.",
+    answer:
+      "Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit nostrud pariatur culpa magna in aute.",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet.",
+    answer:
+      "Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit nostrud pariatur culpa magna in aute.",
+  },
+];
