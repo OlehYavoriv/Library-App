89e2ec95416a317c62e3b2a99c8ceeb3acd1c370
OlehYavoriv <oleh.yavoriv@gmail.com>
2022-12-21 21:04:50 +0200

    Add data

---
+++ src/data/developers.js
@@ -0,0 +1,14 @@
+export const developers = [
+  {
+    name: "Oleh Yavoriv",
+    contact: "#OlehYavoriv",
+  },
+  {
+    name: "Roman Fedyniak",
+    contact: "#RomanFedyniak",
+  },
+  {
+    name: "Ivan Poltavets",
+    contact: "#IvanPoltavets",
+  },
+];
---
+++ src/data/menuItems.js
@@ -0,0 +1,18 @@
+export const menuItems = [
+  {
+    title: "Books",
+    link: "/books",
+  },
+  {
+    title: "Paid Books",
+    link: "/paid-books",
+  },
+  {
+    title: "FAQ",
+    link: "/faq",
+  },
+  {
+    title: "Contacts",
+    link: "/contacts",
+  },
+];
