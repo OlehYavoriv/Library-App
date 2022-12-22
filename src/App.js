3254049615ea146fae84dcfe190d94a4de0ba9f7
OlehYavoriv <oleh.yavoriv@gmail.com>
2022-12-22 17:20:38 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import SignInUser from "./pages/Authorization/SignInUser";
+import Books from "./pages/Books";
+import ContactsPage from "./pages/ContactsPage";
+import FAQ from "./pages/FAQ";
+import MainPage from "./pages/MainPage";
+import NotFoundPage from "./pages/NotFound";
+import PaidBooks from "./pages/PaidBooks";
+
 const App = () => {
   return (
-    <div className="App">
-      <p>Library</p>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<SignInUser />} />
+          
+          <Route path="/home" element={<MainPage />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/paid-books" element={<PaidBooks/>} />
+          <Route path="/contacts" element={<ContactsPage/>} />
+          <Route path="/faq" element={<FAQ/>} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 };
 
