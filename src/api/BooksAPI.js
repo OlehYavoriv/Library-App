e28fb8f14463a3b472f6ab2ae41799aa1a932f9c
OlehYavoriv <oleh.yavoriv@gmail.com>
2022-12-24 00:10:30 +0200

    Add API

---
+++ src/api/BooksAPI.js
@@ -0,0 +1,2 @@
+export const freeBookAPI = "https://desolate-cove-47308.herokuapp.com/api/books/freebooks/";
+export const paidBookAPI = "https://desolate-cove-47308.herokuapp.com/api/books/paidbooks";
