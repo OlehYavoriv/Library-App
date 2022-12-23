550c792dd6c2823c3d238f20284c4f26c28bc988
OlehYavoriv <oleh.yavoriv@gmail.com>
2022-12-24 01:38:32 +0200

    Get books from API

--- src/pages/Books/index.jsx
+++ src/pages/Books/index.jsx
@@ -1,22 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { freeBookAPI } from "../../api/BooksAPI";
 import FreeBook from "../../components/BookItem";
 
-function Books(props) {
+const Books = () => {
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    axios.get(freeBookAPI).then((response) => setBook(response.data));
+  }, []);
+
   return (
     <div>
-        <h2 className="text-center text-3xl text-bold text-sage-green mt-12">Free Books</h2>
+      <h2 className="text-center text-3xl text-bold text-sage-green mt-12">
+        Free Books
+      </h2>
       <div className="place-items-center mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 my-12">
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
+        {book.map((books) => (
+          <FreeBook key={books.id} books={books} />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default Books;
--- src/pages/PaidBooks/index.jsx
+++ src/pages/PaidBooks/index.jsx
@@ -1,22 +1,26 @@
-import React from "react";
-import FreeBook from "../../components/BookItem";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { paidBookAPI } from "../../api/BooksAPI";
+import PaidBooksItem from "../../components/BookItemPaid";
 
-function PaidBooks(props) {
+const PaidBooks = () => {
+  const [paidBook, setPaidBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get(paidBookAPI).then((response) => setPaidBooks(response.data));
+  }, []);
   return (
     <div>
-        <h2 className="text-center text-3xl text-bold text-sage-green mt-12">Paid Books</h2>
+      <h2 className="text-center text-3xl text-bold text-sage-green mt-12">
+        Paid Books
+      </h2>
       <div className="place-items-center mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 my-12">
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
-        <FreeBook />
+        {paidBook.map((books) => (
+          <PaidBooksItem key={books.id} books={books} />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default PaidBooks;
