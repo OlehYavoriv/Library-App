488d20caa0a33e9386250b5dd6ad7abd5a745aba
OlehYavoriv <oleh.yavoriv@gmail.com>
2022-12-24 01:35:56 +0200

    Routes and token

--- src/App.js
+++ src/App.js
@@ -8,20 +8,27 @@ import FAQ from "./pages/FAQ";
 import MainPage from "./pages/MainPage";
 import NotFoundPage from "./pages/NotFound";
 import PaidBooks from "./pages/PaidBooks";
+import useToken from "./hooks/useToken";
+import BookDetails from "./pages/BookDetails";
 
 const App = () => {
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <SignInUser setToken={setToken} />;
+  }
+
   return (
     <>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<SignInUser />} />
-          
           <Route path="/home" element={<MainPage />} />
           <Route path="/books" element={<Books />} />
-          <Route path="/paid-books" element={<PaidBooks/>} />
-          <Route path="/contacts" element={<ContactsPage/>} />
-          <Route path="/faq" element={<FAQ/>} />
+          <Route path="/paid-books" element={<PaidBooks />} />
+          <Route path="/:id" element={<BookDetails />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/faq" element={<FAQ />} />
 
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
